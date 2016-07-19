0db26f472edb64f5e72409d006519d422126abe2
leozhao0709 <zhao434@usc.edu>
2016-07-18 19:01:23 -0700

    test playground

---
+++ TestCode.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let path = NSBundle.mainBundle().pathForResource("cars_simple.plist", ofType: nil)
+
+let tempArray = NSArray(contentsOfFile: path!)
+print(tempArray!)
+
+//let tempArray = NSDictionary(contentsOfFile: path!)
+//print(tempArray)
