37038b57a7646401b96d782001e20271409299e7
lzhao <lzhao@shutterfly.com>
2016-07-20 18:00:52 -0700

    qq界面initial

---
+++ QQ聊天界面/QQ聊天界面/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QQ聊天界面
+//
+//  Created by Lei Zhao on 7/20/16.
+//  Copyright © 2016 Lei Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
