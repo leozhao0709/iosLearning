14ec78742c1fb594c9338d610f45f3a1280833ec
leozhao0709 <zhao434@usc.edu>
2016-07-21 22:40:03 -0700

    qq聊天增加键盘监听

--- QQ聊天界面/QQ聊天界面/ViewController.swift
+++ QQ聊天界面/QQ聊天界面/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
     
     lazy var dataArray: [QQModel] = {
         let path = NSBundle.mainBundle().pathForResource("messages.plist", ofType: nil)
@@ -41,11 +42,60 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
+        self.textField.delegate = self
+        
+        self.registerNotification()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func registerNotification() {
+        
+        //监听键盘即将出现
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillAppear), name: UIKeyboardWillShowNotification, object: nil)
+        
+        //监听键盘即将隐藏
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillDisappear), name: UIKeyboardWillHideNotification, object: nil)
+        
+    }
+    
+    @objc private func keyboardWillDisappear(notification: NSNotification) {
+        
+        let dict = notification.userInfo!
+        
+        let intervalTime = dict[UIKeyboardAnimationDurationUserInfoKey]! as? NSTimeInterval
+        
+        UIView.animateWithDuration(intervalTime!) {
+            self.view.transform = CGAffineTransformIdentity
+        }
+    }
+    
+    @objc private func keyboardWillAppear(notification: NSNotification) {
+//        NSLog("\(notification)")
+        
+        let dict = notification.userInfo!
+        
+//        NSLog("\(dict[UIKeyboardAnimationDurationUserInfoKey]!)")
+        
+//        NSLog("\((dict[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue().height)")
+        
+//        let keyboardEndY = (dict[UIKeyboardFrameEndUserInfoKey]! as! NSValue).CGRectValue().origin.y
+//        
+//        let keyboardBeignY = (dict[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue().origin.y
+//        
+//        NSLog("\(keyboardEndY - keyboardBeignY)")
+        
+        let intervalTime = dict[UIKeyboardAnimationDurationUserInfoKey]! as? NSTimeInterval
+        
+        let keyboardHeight = (dict[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue().height
+        
+        UIView.animateWithDuration(intervalTime!) {
+            self.view.transform = CGAffineTransformMakeTranslation(0, -keyboardHeight)
+        }
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -81,7 +131,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        self.textField.resignFirstResponder()
+        
+        return true
+    }
 
 }
 
