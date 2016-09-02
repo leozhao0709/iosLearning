00c5106e3b16a214bb215f39a7f5a90ff77c3015
lzhao <lzhao@shutterfly.com>
2016-09-02 15:05:06 -0700

    添加self extension

--- 图片裁剪并保存/图片裁剪并保存/ViewController.swift
+++ 图片裁剪并保存/图片裁剪并保存/ViewController.swift
@@ -27,46 +27,50 @@ class ViewController: UIViewController {
         
         let image = UIImage(named: "dst2")
         
-        UIGraphicsBeginImageContextWithOptions((image?.size)!, false, 0.0)
+//        UIGraphicsBeginImageContextWithOptions((image?.size)!, false, 0.0)
+//        
+//        let ctx = UIGraphicsGetCurrentContext()
+//        
+//        let center = CGPointMake((image?.size.width)!/2, (image?.size.height)!/2)
+//        let radius = min((image?.size.width)!, (image?.size.height)!)/2
+//        let path = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * CGFloat(M_PI), clockwise: true)
+//        
+//        CGContextAddPath(ctx, path.CGPath)
+//        
+//        CGContextClip(ctx)
+//        
+//        image?.drawAtPoint(CGPointZero)
+//        
+//        var getImage = UIGraphicsGetImageFromCurrentImageContext()
+//        
+//        UIGraphicsEndImageContext()
+//        
+//        var x: CGFloat = 0
+//        var y: CGFloat = ((image?.size.height)! - 2 * radius)/2
+//        var w: CGFloat = 2 * radius
+//        var h = w
+//        
+//        let scale = UIScreen.mainScreen().scale
+//        
+//        x *= scale
+//        y *= scale
+//        h *= scale
+//        w *= scale
+//        
+//        let imageRef = CGImageCreateWithImageInRect(getImage.CGImage, CGRectMake(x, y, w, h))
+//        
+//        getImage = UIImage(CGImage: imageRef!)
         
-        let ctx = UIGraphicsGetCurrentContext()
         
-        let center = CGPointMake((image?.size.width)!/2, (image?.size.height)!/2)
-        let radius = min((image?.size.width)!, (image?.size.height)!)/2
-        let path = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * CGFloat(M_PI), clockwise: true)
+        let getImage = image!.circleImage()!
         
-        CGContextAddPath(ctx, path.CGPath)
+//        self.imageView.image = getImage
         
-        CGContextClip(ctx)
-        
-        image?.drawAtPoint(CGPointZero)
-        
-        var getImage = UIGraphicsGetImageFromCurrentImageContext()
-        
-        UIGraphicsEndImageContext()
-        
-        var x: CGFloat = 0
-        var y: CGFloat = ((image?.size.height)! - 2 * radius)/2
-        var w: CGFloat = 2 * radius
-        var h = w
-        
-        let scale = UIScreen.mainScreen().scale
-        
-        x *= scale
-        y *= scale
-        h *= scale
-        w *= scale
-        
-        let imageRef = CGImageCreateWithImageInRect(getImage.CGImage, CGRectMake(x, y, w, h))
-        
-        getImage = UIImage(CGImage: imageRef!)
-        
-        self.imageView.image = getImage
-        
+        self.imageView.fadeImageIn(getImage)
         
         
         //保存至相册
-        UIImageWriteToSavedPhotosAlbum(getImage, self,#selector(self.image(_:didFinishSavingWithError:contextInfo:)), nil)
+//        UIImageWriteToSavedPhotosAlbum(getImage, self,#selector(self.image(_:didFinishSavingWithError:contextInfo:)), nil)
         
     }
     
