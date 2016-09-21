ad10746fcc68006847560aa1bd50122f8a72926f
leozhao0709 <zhao434@usc.edu>
2016-09-20 23:48:12 -0700

    封装popmenu

--- WBWeibo/WBWeibo/Classes/Home/Controller/WBHomeViewController.swift
+++ WBWeibo/WBWeibo/Classes/Home/Controller/WBHomeViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class WBHomeViewController: UITableViewController {
 
-    var popMenuWindow: UIWindow?
+    
     
     weak var titleBtn: UIButton?
     
@@ -51,70 +51,16 @@ class WBHomeViewController: UITableViewController {
     }
     
     
-    /// popup menu (use UIWindow)
-    ///
-    /// - parameter fromView: menu point view
-    /// - parameter content:  menu content view
-    func popFromConetnt(fromView: UIView, content: UIView) {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.isHidden = false
-        window.windowLevel = UIWindowLevelAlert
-        self.popMenuWindow = window
-        
-        let cover = UIButton(frame: UIScreen.main.bounds)
-        cover.backgroundColor = UIColor.clear
-        cover.addTarget(self, action: #selector(self.coverClick), for: UIControlEvents.touchUpInside)
-        window.addSubview(cover)
-        
-//        let menuView = UIImageView()
-//        menuView.isUserInteractionEnabled = true
-//        let image = #imageLiteral(resourceName: "popover_background")
-//        menuView.image = image
-//        
-//        cover.addSubview(menuView)
-//        
-//        content.x = 15
-//        content.y = 18
-//        menuView.addSubview(content)
-//        
-//        let menuViewW = content.frame.maxX + content.x
-//        let menuViewH = content.frame.maxY + content.y
-//        menuView.frame = CGRect(x: 0, y: 0, width: menuViewW, height: menuViewH)
-        
-        let menuView = WBHomeMenuView()
-        cover.addSubview(menuView)
-        
-        let resultFrame = window.convert(fromView.frame, from: fromView.superview)
-        menuView.y = resultFrame.maxY
-        menuView.centerX = resultFrame.origin.x + resultFrame.size.width * 0.5
-    }
-    
-    func popMenu(fromView view: UIView, menuView: UIView) {
-        let view = UIView(frame: UIScreen.main.bounds)
-        view.backgroundColor = UIColor.black
-        view.addSubview(menuView)
-        
-        self.view.addSubview(view)
-        
-        self.view.bringSubview(toFront: view)
-    }
-    
-    @objc private func coverClick() {
-        self.popMenuWindow = nil
-        self.titleBtn?.isSelected = !(self.titleBtn?.isSelected)!
-    }
+
     
     @objc private func titleBtnClick(btn: UIButton) {
         btn.isSelected = !btn.isSelected
         
-        let tb = UITableView()
-        tb.frame.size = CGSize(width: 150, height: 200)
+        let menuView = WBHomeMenuView()
         
-//        self.popFromConetnt(fromView: self.testBtn!, content: tb)
-        self.popFromConetnt(fromView: btn, content: tb)
-        
-//        let menuView = WBHomeMenuView()
-//        self.popMenu(fromView: btn, menuView: menuView)
+        WBPopMenu.popFromView(fromView: btn, menuContentView: menuView) {
+            btn.isSelected = !btn.isSelected
+        }
         
     }
     
---
+++ WBWeibo/WBWeibo/Classes/Home/View/WBPopMenu.swift
@@ -0,0 +1,54 @@
+//
+//  WBPopMenuView.swift
+//  WBWeibo
+//
+//  Created by Lei Zhao on 9/20/16.
+//  Copyright © 2016 Lei Zhao. All rights reserved.
+//
+
+import UIKit
+
+class WBPopMenu: NSObject {
+    
+    static var popMenuWindow: UIWindow?
+    
+    private static var dismiss: (()->Void)?
+    
+    /// popup menu (use UIWindow)
+    ///
+    /// - parameter fromView: menu point view
+    /// - parameter contentView:  menu content view
+    /// - parameter dismiss: menu dismiss closure
+    static func popFromView(fromView: UIView, menuContentView contentView: UIView, dismiss: (()->Void)?) {
+        
+        WBPopMenu.dismiss = dismiss
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.isHidden = false
+        window.windowLevel = UIWindowLevelAlert
+        WBPopMenu.popMenuWindow = window
+        
+        
+        let cover = UIButton(frame: UIScreen.main.bounds)
+        cover.backgroundColor = UIColor.clear
+        cover.addTarget(self, action: #selector(coverClick), for: UIControlEvents.touchUpInside)
+        window.addSubview(cover)
+        
+        cover.addSubview(contentView)
+        
+        let resultFrame = window.convert(fromView.frame, from: fromView.superview)
+        contentView.y = resultFrame.maxY
+        contentView.centerX = resultFrame.origin.x + resultFrame.size.width * 0.5
+        
+    }
+    
+    
+    @objc static func coverClick() {
+        WBPopMenu.popMenuWindow = nil
+        
+        if let dismiss = self.dismiss {
+            dismiss()
+        }
+    }
+    
+}
