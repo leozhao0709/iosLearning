4b216237a570227566e430898fc3a9b9d6c8a4d8
lzhao <lzhao@shutterfly.com>
2016-09-22 16:17:56 -0700

    default center view

---
+++ WBWeibo/WBWeibo/Classes/Main/View/WBDefaultCenterView.swift
@@ -0,0 +1,21 @@
+//
+//  WBDefaultCenterView.swift
+//  WBWeibo
+//
+//  Created by Lei Zhao on 9/22/16.
+//  Copyright © 2016 Lei Zhao. All rights reserved.
+//
+
+import UIKit
+
+class WBDefaultCenterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
